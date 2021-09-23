cf94e93315840c6153020f59133b628509765b58
syedrizvi786 <syedhassaanalirizvi@gmail.com>
2021-09-23 19:59:46 -0400

    testing

--- js/app.js
+++ js/app.js
@@ -1,28 +1,52 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const form = document.querySelector('form.add');
+    const addButton = document.querySelector('#addButton');
+    const itemToAdd = document.querySelector('#itemToAdd');
+    const ul = document.querySelector('.todo');
+    const body = document.querySelector('body');
+    const app = document.querySelector(".app");
 
-const form = document.querySelector('form.add');
-const itemToAdd = document.querySelector('#itemToAdd');
-const ul = document.querySelector('.todo');
-//listener
-form.addEventListener('submit',function (){
-    const newItem = createItem(itemToAdd.value);
-    ul.appendChild(newItem);
-})
-//listener2/hk
-function createItem(val){
-    const item = document.createElement('li');
-    const span = document.createElement('span');
-    const delBtn = document.createElement('button');
 
-    span.textContent = val;
-    delBtn.textContent = 'Delete';
-    delBtn.classList.add('btn-link');
-
-    item.appendChild(span);
-    item.appendChild(delBtn);
-
-    delBtn.addEventListener('click',function (){
-        item.parentNode.removeChild(item);
+    app.addEventListener('click', function(event){
+        event.stopPropagation();
     })
 
-    return item;
-}
+
+
+    body.addEventListener('click', function(event){
+        ul.innerHTML = "";
+    })
+
+
+    form.addEventListener('submit',function (){
+        event.preventDefault();
+        if(itemToAdd.value){
+            const newItem = createItem(itemToAdd.value);
+            ul.appendChild(newItem);
+            itemToAdd.value = '';
+            itemToAdd.focus();
+        }
+
+    })
+//listener2/hkfs
+    function createItem(val){
+        const item = document.createElement('li');
+        const span = document.createElement('span');
+        const delBtn = document.createElement('button');
+
+        span.textContent = val;
+        delBtn.textContent = 'Delete';
+        delBtn.classList.add('btn-link');
+
+        item.appendChild(span);
+        item.appendChild(delBtn);
+
+        delBtn.addEventListener('click',function (){
+            item.parentNode.removeChild(item);
+        })
+
+        return item;
+    }
+
+});
+
